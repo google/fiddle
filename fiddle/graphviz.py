cf2a1007fd76d9d95c072fa773fa267c6de6b4e6
Edward Loper <edloper@google.com>
2022-05-27 05:58:47 -0700

    [fiddle/graphviz] Display shared values as separate nodes in the graphviz graph.  E.g., if a configuration has a shared List or user-defined object, then show that shared object as a separate node.  This change also makes it possible to have the top-level node rendered by `graphviz.render()` be a list of Configs (or any other nested structure).
    PiperOrigin-RevId: 451375839

--- fiddle/graphviz.py
+++ fiddle/graphviz.py
@@ -19,11 +19,12 @@ import abc
 import html
 import itertools
 
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union, Set
 
 from fiddle import config as fdl
 from fiddle import tagging
 from fiddle.codegen import formatting_utilities
+from fiddle.experimental import daglish
 import graphviz
 import typing_extensions
 
@@ -43,6 +44,8 @@ _BUILDABLE_INSTANCE_COLORS = [
     '#00bfff',  # deepskyblue
     '#7b68ee',  # mediumslateblue
 ]
+
+_DEFAULT_HEADER_COLOR = '#eeeeee'
 
 
 class GraphvizRendererApi(typing_extensions.Protocol):
@@ -110,16 +113,19 @@ class _GraphvizRenderer:
             'color': '#00000030',
             'penwidth': '3',
         })
-    self._configs = {}
+    self._node_id_by_value_id = {}
     # The id of the config currently being rendered. This is obtained from
-    # _config_counter and used in _render_config and _render_nested_config.
-    self._current_id = None
+    # _config_counter and used in _add_node_for_value and _render_nested_value.
+    self._current_id: Optional[int] = None
     self._config_counter = itertools.count()
     # Used to assign unique port names to table cells for nested configs.
     self._port_counter = itertools.count()
+    # Ids of values reachable via multiple paths.
+    self._shared_value_ids: Set[int] = set()
 
-  def _color(self, index: int):
-    return self._instance_colors[index % len(self._instance_colors)]
+  def _color(self, value):
+    node_id = self._node_id_by_value_id[id(value)]
+    return self._instance_colors[node_id % len(self._instance_colors)]
 
   def tag(self, tag: str, **kwargs) -> Callable[[Any], str]:
     """Returns a function that creates HTML tags of type `tag`.
@@ -154,128 +160,184 @@ class _GraphvizRenderer:
 
     return tag_fn
 
-  def _header_row(self, header, colspan=2, bgcolor='#eeeeee', style='solid'):
+  def _header_row(self,
+                  header,
+                  colspan: int = 2,
+                  bgcolor: str = _DEFAULT_HEADER_COLOR,
+                  style: str = 'solid'):
     """Constructs a header table row."""
     tr = self.tag('tr')
     header_td = self.tag('td', colspan=colspan, bgcolor=bgcolor, style=style)
     return tr(header_td(header))
 
-  def render(self, config: fdl.Buildable) -> graphviz.Graph:
-    """Renders the given `config`, recursively rendering any nested configs."""
-    self._render_config(config)
+  def render(self, value: Any) -> graphviz.Graph:
+    """Renders the given value, recursively rendering any nested values."""
+    self._find_shared_value_ids(value)
+    self._add_node_for_value(value)
     dot = self._dot
     self._clear()
     return dot
 
-  def _render_config(self, config):
-    """Adds `config` to the graph and renders it as a table."""
-    if id(config) in self._configs:
-      return  # Don't do anything if `config` has already been rendered.
+  def _find_shared_value_ids(self, root: Any):
+    """Adds ids of shared values in `root` to `self._shared_value_ids`."""
 
+    visited_ids = set()
+
+    def visit(path, value):
+      del path  # Unused.
+      if not daglish.is_memoizable(value):
+        return value
+      if id(value) in visited_ids:
+        self._shared_value_ids.add(id(value))
+        return value
+      visited_ids.add(id(value))
+      return (yield)
+
+    daglish.traverse_with_path(visit, root)
+
+  def _add_node_for_value(self, value: Any):
+    """Adds a node for `value` to the graphviz graph `self._dot`.
+
+    Also sets self._node_id_by_value[id(value)] to the node's graphviz id,
+    and sets `_current_id` to the node's graphviz id.
+
+    Args:
+      value: The value that should be rendered.
+    """
+    if id(value) in self._node_id_by_value_id:
+      return  # Don't do anything if we have already rendered this node.
     last_id = self._current_id
     self._current_id = next(self._config_counter)
-    self._configs[id(config)] = self._current_id
+    self._node_id_by_value_id[id(value)] = self._current_id
+    html_label = self._render_value(value, self._color(value))
+    if not (isinstance(value, (fdl.Buildable, dict, list, tuple)) or
+            daglish.is_namedtuple_instance(value)):
+      table = self.tag('table')
+      tr = self.tag('tr')
+      td = self.tag('td')
+      html_label = table([
+          self._header_row(
+              type(value).__name__, bgcolor=self._color(value), colspan=1),
+          tr(td(html_label))
+      ])
+    self._dot.node(str(self._current_id), f'<{html_label}>')
 
+    self._current_id = last_id
+
+  def _render_tagged_value(self, tagged_value: fdl.Buildable,
+                           bgcolor: str) -> str:
+    """Returns an HTML string rendering `tagged_value`."""
+    type_font = self.tag('font', point_size=8)
+    type_name = tagged_value.__class__.__name__
+    key_names = ', '.join(repr(key.name) for key in tagged_value.tags)
+    title = (type_font(html.escape(f'{type_name}: {key_names}')) + '&nbsp;')
+    header = self._header_row(title, colspan=1, bgcolor=bgcolor, style='solid')
+
+    table = self.tag('table')
+    tr = self.tag('tr')
+    value_td = self.tag('td', align='left')
+    rendered_value = self._render_nested_value(tagged_value.value)
+    return table([header, tr([value_td(rendered_value)])])
+
+  def _render_config(self, config: fdl.Buildable, bgcolor: str) -> str:
+    """Returns an HTML string rendering the Buildable `config`."""
     # Generate the header row.
-    bgcolor = self._color(self._current_id)
     style = 'dashed' if isinstance(config, fdl.Partial) else 'solid'
     type_font = self.tag('font', point_size=8)
     type_name = config.__class__.__name__
-    if isinstance(config, tagging.TaggedValue):
-      key_names = ', '.join(repr(key.name) for key in config.tags)
-      title = (type_font(html.escape(f'{type_name}: {key_names}')) + '&nbsp;')
-      header = self._header_row(title, colspan=1, bgcolor=bgcolor, style=style)
-    else:
-      fn_or_cls_name = config.__fn_or_cls__.__name__
-      title = (
-          type_font(html.escape(f'{type_name}:')) + '&nbsp;' +
-          html.escape(fn_or_cls_name))
-      header = self._header_row(title, bgcolor=bgcolor, style=style)
+    fn_or_cls_name = config.__fn_or_cls__.__name__
+    title = (
+        type_font(html.escape(f'{type_name}:')) + '&nbsp;' +
+        html.escape(fn_or_cls_name))
+    header = self._header_row(title, bgcolor=bgcolor, style=style)
 
     # Generate the arguments table.
-    if isinstance(config, tagging.TaggedValue):
-      table = self.tag('table')
-      tr = self.tag('tr')
-      value_td = self.tag('td', align='left')
-      label = table([header, tr([value_td(self._render_value(config.value))])])
-    elif config.__arguments__:
+    if config.__arguments__:
       label = self._render_dict(
           config.__arguments__, header=header, key_format_fn=str)
     else:
       table = self.tag('table')
       italics = self.tag('i')
       label = table([header, self._header_row(italics('no arguments'))])
-    self._dot.node(str(self._current_id), f'<{label}>')
+    return label
 
-    self._current_id = last_id
-
-  def _render_value(self, value: Any):
-    """Renders an arbitrary value inside a `Config` rendering."""
+  def _render_value(self, value: Any, color=_DEFAULT_HEADER_COLOR) -> str:
+    """Returns an HTML string rendering `value`."""
     if value is tagging.NO_VALUE:
       return self.tag('i')('tagging.NO_VALUE')
     elif isinstance(value, CustomGraphvizBuildable):
       return value.__render_value__(self)
+    elif isinstance(value, tagging.TaggedValue):
+      return self._render_tagged_value(value, color)
     elif isinstance(value, fdl.Buildable):
-      return self._render_nested_config(value)
+      return self._render_config(value, color)
     elif isinstance(value, dict):
       return self._render_dict(
-          value, header=self._header_row(type(value).__name__))
-    elif isinstance(value, tuple) and hasattr(value, '_asdict'):
-      # If it has an "_asdict" attribute it's almost certainly a `NamedTuple`.
-      # Unfortunately there is no direct way of testing whether an object is a
-      # `NamedTuple`, since all `NamedTuple`s are direct subclasses of `tuple`.
+          value, header=self._header_row(type(value).__name__, bgcolor=color))
+    elif daglish.is_namedtuple_instance(value):
       return self._render_dict(
           value._asdict(),
-          header=self._header_row(type(value).__name__),
+          header=self._header_row(type(value).__name__, bgcolor=color),
           key_format_fn=str)
     elif isinstance(value, (list, tuple)):
-      return self._render_sequence(value)
+      return self._render_sequence(value, color)
     else:
       return self._render_leaf(value)
 
-  def _render_nested_config(self, config: fdl.Buildable) -> str:
-    """Renders a nested config as a rectangle, linked to its parent config.
+  def _render_nested_value(self, value: Any):
+    """Returns an HTML string rendering `value` inside another object.
 
-    This function renders the appearance of a config when it is nested inside
-    another config (and potentially inside a list/dictionary within the config).
-    First, `config` is rendered as its own separate unnested graph node (if it
-    hasn't been already). The nested rendering is then just a single-celled
-    table whose color matches the header color of the separate node rendering.
-    The nested cell is then connected via an edge to the separate rendering.
+    If `value` is a `Buildable` or a shared memoizable value, then it is
+    rendered as its own separate unnested graph node (if it hasn't been
+    already). The nested rendering is then just a single-celled table whose
+    color matches the header color of the separate node rendering. The nested
+    cell is then connected via an edge to the separate rendering.
+
+    Otherwise, this returns `self._render_value(value)`.
 
     Args:
-      config: The nested `fdl.Buildable` to render.
+      value: The nested value to render.
 
     Returns:
-      The HTML markup for the resulting table representing `config`.
+      The HTML markup for the rectangle (a single-celled table) that is
+      connected by an edge to the node for `value`.
     """
-    # First, add the config to the graph. This will add a separate node and
+    # If this is not a Buildable or shared value, then render it using
+    # _render_value.
+    if not (id(value) in self._shared_value_ids or
+            (isinstance(value, fdl.Buildable) and
+             not isinstance(value, CustomGraphvizBuildable))):
+      return self._render_value(value)
+
+    # First, add the value to the graph. This will add a separate node and
     # render it as a separate table (but is a no-op if it's already added).
-    self._render_config(config)
-    # Look up the config id for the config, and get a new unique port name to
+    self._add_node_for_value(value)
+
+    # Look up the node id for the value, and get a new unique port name to
     # use for the nested cell (see below).  The port allows the edge to go
-    # "inside" the parent config and connect directly to the table cell.
-    config_id = self._configs[id(config)]
+    # "inside" the parent value's node and connect directly to the table cell.
+    node_id = self._node_id_by_value_id[id(value)]
     port = next(self._port_counter)
-    # Now add an edge in the graph to the parent config. The direction here
+    # Now add an edge in the graph to the parent value. The direction here
     # determines the order in which the graph is layed out when using the
-    # default "dot" layout engine, so putting the parent config first lays the
+    # default "dot" layout engine, so putting the parent value first lays the
     # graph out from root to children.
-    self._dot.edge(f'{self._current_id}:{port}:c', f'{config_id}:c')
+    self._dot.edge(f'{self._current_id}:{port}:c', f'{node_id}:c')
 
     # Return a table with a single colored cell, using the port name from above.
-    style = 'dashed' if isinstance(config, fdl.Partial) else 'solid'
+    style = 'dashed' if isinstance(value, fdl.Partial) else 'solid'
     table = self.tag('table', style=style)
     tr = self.tag('tr')
-    td = self.tag('td', port=port, bgcolor=self._color(config_id), style=style)
+    td = self.tag('td', port=port, bgcolor=self._color(value), style=style)
     return table(tr(td('')))
 
-  def _render_sequence(self, sequence: Union[List[Any], Tuple[Any]]) -> str:
+  def _render_sequence(self, sequence: Union[List[Any], Tuple[Any]],
+                       color) -> str:
     """Renders the given sequence (list or tuple) as an HTML table."""
     table = self.tag('table')
     tr = self.tag('tr')
     td = self.tag('td')
-    index_td = self.tag('td', cellpadding=0, bgcolor='#eeeeee')
+    index_td = self.tag('td', cellpadding=0, bgcolor=_DEFAULT_HEADER_COLOR)
     index_font = self.tag('font', point_size=6)
 
     if not sequence:
@@ -283,11 +345,14 @@ class _GraphvizRenderer:
 
     cells, indices = [], []
     for i, value in enumerate(sequence):
-      cells.append(td(self._render_value(value)))
+      cells.append(td(self._render_nested_value(value)))
       indices.append(index_td(index_font(i)))
     row_stride = self._max_sequence_elements_per_row
     colspan = min(len(sequence), row_stride)
-    rows = [self._header_row(type(sequence).__name__, colspan=colspan)]
+    rows = [
+        self._header_row(
+            type(sequence).__name__, colspan=colspan, bgcolor=color)
+    ]
     for i in range(0, len(sequence), row_stride):
       rows.extend([tr(cells[i:i + row_stride]), tr(indices[i:i + row_stride])])
     return table(rows)
@@ -310,13 +375,13 @@ class _GraphvizRenderer:
     """
     table = self.tag('table')
     tr = self.tag('tr')
-    key_td = self.tag('td', align='right', bgcolor='#eeeeee')
+    key_td = self.tag('td', align='right', bgcolor=_DEFAULT_HEADER_COLOR)
     value_td = self.tag('td', align='left')
 
     rows = [header] if header is not None else []
     for key, value in dict_.items():
       key = key_format_fn(key)
-      rows.append(tr([key_td(key), value_td(self._render_value(value))]))
+      rows.append(tr([key_td(key), value_td(self._render_nested_value(value))]))
     return table(rows)
 
   def _render_leaf(self, value: Any) -> str:
@@ -325,7 +390,7 @@ class _GraphvizRenderer:
     return html.escape(repr(value))
 
 
-def render(config: fdl.Buildable) -> graphviz.Graph:
+def render(config: Any) -> graphviz.Graph:
   """Renders the given `config` as a `graphviz.Graph`.
 
   Each config is rendered as a table of keys and values (with a header row).
@@ -335,7 +400,8 @@ def render(config: fdl.Buildable) -> graphviz.Graph:
   edges to parent configs.
 
   Args:
-    config: The config to render.
+    config: The `fdl.Buildable` (or nested structure of `fdl.Buildable`s) to
+      render.
 
   Returns:
     A `graphviz.Graph` object containing the resulting rendering of `config`.
