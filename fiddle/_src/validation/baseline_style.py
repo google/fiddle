42e0f71f8cf3ff74b9102cb8ddc207fc11dbb46b
Ashish Shenoy <ashishenoy@google.com>
2023-07-12 00:44:45 -0700

    Implement readability checks for baseline configurations.
    This commit adds support for the following readability validation checks for baseline configs and their attributes:
    - at most `max_writes_per_attribute` writes to each attribute, so not much overriding.
    - at most `max_config_source_files` files writing to a config attribute.
    - attributes specified in `ignore_attributes` are ignored from the above readability validations.
    
    PiperOrigin-RevId: 547415512

---
+++ fiddle/_src/validation/baseline_style.py
@@ -0,0 +1,150 @@
+# coding=utf-8
+# Copyright 2022 The Fiddle-Config Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Programmatic checks for the style of a baseline configuration.
+
+"Baseline configurations" are configurations for baseline or flagship models.
+They will be read by a large number of people, so keeping them simple and
+readable is important.
+"""
+
+from typing import Any, Dict, List, Optional
+
+from fiddle._src import config as config_lib
+from fiddle._src import daglish
+from fiddle._src import history
+
+
+def _check_max_writes_per_attribute(
+    attr_history: List[history.HistoryEntry],
+    max_writes_per_attribute: int,
+    path_str: str,
+) -> None:
+  """Style check for the max number of writes to an attribute.
+
+  Checks the max number of writes to an attribute doesn't exceed a certain
+  threshold.
+
+  Args:
+    attr_history: The list of history entries for a given config attribute.
+    max_writes_per_attribute: Threshold for maximum number of writes to a given
+      config attribute.
+    path_str: A string that would be returned by `daglish.path_str(p)` for some
+      path p.
+  """
+  value_history = [
+      entry
+      for entry in attr_history
+      if entry.kind == history.ChangeKind.NEW_VALUE
+  ]
+
+  filenames = set()
+  for history_entry in attr_history:
+    location: history.Location = history_entry.location
+    filenames.add(location.filename)
+
+  debug_str = ", ".join(f"{filename}" for filename in filenames)
+
+  if len(value_history) > max_writes_per_attribute:
+    raise ValueError(
+        f"{len(value_history)} value(s) written to <config>{path_str} in files:"
+        f" {debug_str}. Please limit the number of overrides of this config"
+        f" attribute to {max_writes_per_attribute} to maintain baseline"
+        " configuration readability."
+    )
+
+
+def _check_max_config_source_files(
+    sample_paths_by_file: Dict[str, str],
+    max_config_source_files: int,
+) -> None:
+  """Style check for number of files where an attribute is written.
+
+  Checks the max number of files where an attribute is written, doesn't exceed a
+  certain threshold.
+
+  Args:
+    sample_paths_by_file: A dictionary of filenames mapped to string that would
+      be returned by `daglish.path_str(p)` for some path p for a config's
+      attribute.
+    max_config_source_files: Threshold for maximum number of files producing
+      `config`.
+  """
+  if len(sample_paths_by_file) > max_config_source_files:
+    debug_str = ", ".join(
+        f"{filename} (example: wrote to <config>{path})"
+        for filename, path in sample_paths_by_file.items()
+    )
+    raise ValueError(
+        f"More than {max_config_source_files} file(s) produced this"
+        " config. For baseline configurations, please ensure that you don't"
+        " have a big hierarchy of files each overriding values in a config,"
+        " because this is harder to read for new users. The files which have"
+        f" written to this config are: {debug_str}."
+    )
+
+
+def check_baseline_style(
+    config: Any,
+    *,
+    max_config_source_files: int = 3,
+    max_writes_per_attribute: int = 2,
+    ignore: Optional[List[Any]] = None,
+) -> None:
+  """Checks that a configuration is style-conformant.
+
+  Args:
+    config: fdl.Buildable object, or collection which may include Buildable
+      objects.
+    max_config_source_files: Threshold for maximum number of files producing
+      `config`.
+    max_writes_per_attribute: Threshold for maximum number of writes to
+      `config`'s attributes.
+    ignore: An optional list of config sub-objects that are ignored for these
+      readability validations/style checks.
+  """
+
+  # filename --> sample path.
+  sample_paths_by_file = {}
+
+  ignore = ignore or []
+  ignore_ids = {id(obj) for obj in ignore}
+
+  def traverse(value: Any, state: daglish.State) -> Any:
+    if id(value) in ignore_ids:
+      return
+
+    if isinstance(value, config_lib.Buildable):
+      for attr_name, attr_value in config_lib.ordered_arguments(value).items():
+        if id(attr_value) in ignore_ids:
+          continue
+
+        attr_history = value.__argument_history__[attr_name]
+        path_str = daglish.path_str(state.current_path)
+
+        _check_max_writes_per_attribute(
+            attr_history, max_writes_per_attribute, path_str
+        )
+
+        for history_entry in attr_history:
+          location: history.Location = history_entry.location
+          sample_paths_by_file.setdefault(location.filename, path_str)
+
+    if state.is_traversable(value):
+      state.flattened_map_children(value)
+
+  daglish.MemoizedTraversal.run(traverse, config)
+
+  _check_max_config_source_files(sample_paths_by_file, max_config_source_files)
---
+++ fiddle/_src/validation/baseline_style_test.py
@@ -0,0 +1,139 @@
+# coding=utf-8
+# Copyright 2022 The Fiddle-Config Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for baseline_style."""
+
+
+from absl.testing import absltest
+from fiddle._src.testing.example import fake_encoder_decoder
+from fiddle._src.validation import fake_experiment
+from fiddle.validation import baseline_style
+
+
+class BaselineStyleTest(absltest.TestCase):
+
+  def test_check_baseline_style_okay(self):
+    config = fake_encoder_decoder.fixture.as_buildable()
+    baseline_style.check_baseline_style(
+        config=config, max_config_source_files=1
+    )
+
+  def test_check_baseline_style_too_many_files(self):
+    config = fake_experiment.fake_experiment()
+    with self.assertRaisesRegex(
+        ValueError,
+        r"More than 1 file\(s\) produced this config.*files which have"
+        r" written.*fake_encoder_decoder.*"
+        r"fake_experiment.*encoder\.attention",
+    ):
+      baseline_style.check_baseline_style(
+          config=config, max_config_source_files=1
+      )
+
+  def test_check_baseline_style_too_many_overrides(self):
+    config = fake_experiment.fake_experiment()
+    # override dtype one more time to int64 for a total of 3 overrides
+    config.encoder.attention.dtype = "int64"
+    with self.assertRaisesRegex(
+        ValueError,
+        r"3 value\(s\) written to <config>.*Please limit the number of"
+        r" overrides of this config attribute to 2 to maintain baseline"
+        r" configuration readability.",
+    ):
+      baseline_style.check_baseline_style(
+          config=config,
+          max_config_source_files=3,
+          max_writes_per_attribute=2,
+      )
+
+  def test_check_baseline_style_too_many_overrides_for_ignored_attribute(self):
+    config = fake_experiment.fake_experiment()
+    # override dtype one more time to int64 for a total of 3 overrides
+    config.encoder.attention.dtype = "int64"
+    # Here we set both max_config_source_files and max_writes_per_attribute
+    # to 1. This should cause this test to fail as in the other test cases
+    # above, but since we ignore the attribute ".encoder.attention.dtype"
+    # validation checks for readabiilty succeed.
+    baseline_style.check_baseline_style(
+        config=config,
+        max_config_source_files=1,
+        max_writes_per_attribute=1,
+        ignore=[config.encoder.attention.dtype],
+    )
+
+  def test_check_baseline_style_ignored_attribute(self):
+    # Create a config and write to its encoder.embedders attribute which has
+    # been set once before in fake_encoder_decoder.py
+    config = fake_encoder_decoder.fixture.as_buildable()
+    config.encoder.embedders["tokens"].dtype = "float64"
+
+    # Setting max_writes_per_attribute to 1 should throw an error since the
+    # encoder.embedders attribute is being written to twice.
+    with self.assertRaisesRegex(
+        ValueError,
+        r"2 value\(s\) written to <config>.*Please limit the number of"
+        r" overrides of this config attribute to 1 to maintain baseline"
+        r" configuration readability.",
+    ):
+      baseline_style.check_baseline_style(
+          config=config,
+          max_config_source_files=3,
+          max_writes_per_attribute=1,
+      )
+
+    # However, if the encoder.embedders is set in ignored_attributes, all the
+    # validations should succeed.
+    baseline_style.check_baseline_style(
+        config=config,
+        max_config_source_files=3,
+        max_writes_per_attribute=1,
+        ignore=[
+            config.encoder.embedders["tokens"].dtype,
+            config.decoder.embedders["tokens"].dtype,
+        ],
+    )
+
+  def test_check_baseline_style_ignored_non_attribute(self):
+    # Create a config and write to its encoder.embedders attribute which has
+    # been set once before in fake_encoder_decoder.py
+    config = fake_encoder_decoder.fixture.as_buildable()
+    config.encoder.embedders["tokens"].dtype = "float64"
+    # Create config wrappers that wrap `config`
+    foo = {"foo": config}
+    bar = {"bar": foo}
+    qux = {"qux": bar}
+
+    # Ignoring `bar` here should cause the style checks to not recurse into
+    # `foo`, thereby passing the style check that enforces a maximum of 1 write
+    # to `config`'s attributes
+    baseline_style.check_baseline_style(
+        qux, max_writes_per_attribute=1, ignore=[bar]
+    )
+
+    # Not ignoring any sub-objects of the `config` wrapper `qux` here should
+    # result in the style check code recursing into all attributes of `config`
+    # and discover the violation of the style check that enforces a maximum of
+    # 1 write to `config`'s attributes
+    with self.assertRaisesRegex(
+        ValueError,
+        r"2 value\(s\) written to <config>.*Please limit the number of"
+        r" overrides of this config attribute to 1 to maintain baseline"
+        r" configuration readability.",
+    ):
+      baseline_style.check_baseline_style(qux, max_writes_per_attribute=1)
+
+
+if __name__ == "__main__":
+  absltest.main()
